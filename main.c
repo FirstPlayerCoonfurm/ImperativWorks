80e78edc0a0ce8c4bc4e311e458f993fdfe72bd1
FirstPlayerCoonfurm <119936322+FirstPlayerCoonfurm@users.noreply.github.com>
2024-09-04 21:37:45 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int Work_1(){
     int a, b, res;
@@ -11,7 +10,7 @@ int Work_1(){
     else{
         printf("Are you dumb?");
     };
-    return 0;
+    //return 0;
 };
 
 int Work_2(){
@@ -20,8 +19,8 @@ int Work_2(){
     if(a >= 0 && b <= 100){
         res = a * b;
         printf("%d\n", res);
-        printf("%d\n", a % b);
-        printf("\t%d", b % a);
+        printf("%d", a % b);
+        printf("\t%d\n", b % a);
     }
     else{
         printf("Are you dumb?");
@@ -76,7 +75,7 @@ int Work_5(){
     return 0;
 };
 
-bool Work_6(){
+int Work_6(){
     int num;
     scanf("%d" ,&num);
 
@@ -84,26 +83,57 @@ bool Work_6(){
         for(int i = 2; i < num; i++){
             if(num % i == 0){
                 printf("NO");
-                return false;
             }else{
                 printf("YES\n");
-                return true;
             };
         };
     };
+    return 0;
 };
 
 int Work_7(){
-    double num_1, num_2;
-    scanf("%lf%lf", &num_1, &num_2);
-
-    if(num_1 <= 10000 && num_2 <= 10000){
-        printf();
-    }
-    else
-        printf("Are you wining son?");
- return 0;
+    int num_a, num_b;
+    scanf("%d%d", &num_a, &num_b);
+    printf("\n%d\t", num_a / num_b);
+    printf("%d\t", (num_a % num_b) + (num_a / num_b));
+    printf("%d\t", num_a / num_b);
+    printf("%d\n", num_a % num_b);
 };
+
+int Work_8(){
+    int size;
+
+    scanf("%d", &size);
+    int Mass[size];
+     for(int i = 0; i < size; i++){
+        scanf("%d", &Mass[i]);
+    };
+    int min_num, max_num = Mass[0];
+    int index_min, index_max = 0;
+
+    for(int i = 0; i < size; i++){
+        if(Mass[i] < min_num){
+            min_num = Mass[i];
+            index_min = i;
+        }
+        if(Mass[i] > max_num){
+            max_num = Mass[i];
+            index_max = i;
+        }
+    }
+    printf("%d%d%d%d\n", min_num, index_min, max_num, index_max);
+};
+
+int Work_9(){
+    int N, M, P, K, L;
+
+    scanf("%d%d%d%d%d", &N, &M, &P, &K, &L);
+    if(N > 500)
+        printf("Are you wining?");
+
+    
+}
+
 
 int main(){
     //Work_1();
@@ -112,5 +142,8 @@ int main(){
     //Work_4();
     //Work_5();
     //Work_6();
+    //Work_7();
+    //Work_8();
+    Work_9();
     return 0;
 }
